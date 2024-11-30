af6377dccda5580cb09268b634e407fece36fbd6
Sheng1994 <sxl2408@mavs.uta.edu>
2024-11-30 02:00:26 -0600

    Liuting code

--- cmake-build-debug/test/CTestTestfile.cmake
+++ cmake-build-debug/test/CTestTestfile.cmake
@@ -4,3 +4,5 @@
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
+add_test(synthesis_LUT "synthesis_LUT")
+set_tests_properties(synthesis_LUT PROPERTIES  _BACKTRACE_TRIPLES "/home/sheng/CLionProjects/percy/test/CMakeLists.txt;63;add_test;/home/sheng/CLionProjects/percy/test/CMakeLists.txt;0;")
